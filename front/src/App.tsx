1fbae777b71cc331dfeec999c4b1513b61a5d121
Joel Butler <90467562+joelebutler@users.noreply.github.com>
2026-03-28 13:11:59 -0400

    Themes implemented

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,23 @@
 import { sharedString } from "@shared/shared";
+import { useEffect, useState } from "react";
+import { Button } from "@ariakit/react";
+
+type Theme = "default" | "dark" | "sunset";
 
 function App() {
+  const [theme, setTheme] = useState<Theme>(
+    (localStorage.getItem("theme") as Theme) || "default",
+  );
+  useEffect(() => {
+    console.log(theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <>
+      {/* <Button onClick={() => setTheme("default")}>Default theme</Button>
+      <Button onClick={() => setTheme("dark")}>Dark theme</Button>
+      <Button onClick={() => setTheme("sunset")}>Sunset theme</Button> */}
       <h1>I am a frontend!</h1>
       <p>I share with my backend. {sharedString}</p>
     </>
