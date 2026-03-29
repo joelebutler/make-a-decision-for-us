24197c13f75e5f56bb2039dc229c5383d6bfc28b
Joel Butler <90467562+joelebutler@users.noreply.github.com>
2026-03-28 21:19:41 -0400

    max select height

--- front/src/components/Select.tsx
+++ front/src/components/Select.tsx
@@ -21,26 +21,28 @@ function Select({ label, value, items, onChange, className }: SelectProps) {
     if (typeof val === "string") onChange(val);
   };
   return (
-    <div className={className || "w-full"}>
+    <div className={`${className || "w-full"}`}>
       <Ariakit.SelectProvider value={value} setValue={handleChange}>
-        <Ariakit.SelectLabel className="block text-lg font-medium mb-2">
+        <Ariakit.SelectLabel className="block text-lg font-medium mb-2 ">
           {label}
         </Ariakit.SelectLabel>
         <Ariakit.Select className="w-full px-4 py-2 rounded border border-brand/30 bg-surface/80 text-text font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-brand transition-colors duration-150 cursor-pointer flex items-center justify-between gap-2" />
-        <Ariakit.SelectPopover className="mt-2 rounded shadow-lg bg-white dark:bg-surface/90 border border-brand/20 py-2 z-50 animate-fade-in">
-          {items.map((item) => (
-            <Ariakit.SelectItem
-              key={item.value}
-              value={item.value}
-              disabled={item.disabled}
-              className={
-                item.className ||
-                `block w-full px-4 py-2 text-left rounded transition-colors duration-100 ${item.disabled ? "text-text/40 cursor-not-allowed opacity-60" : "text-text hover:bg-brand/10 focus:bg-brand/10 focus:outline-none capitalize"}`
-              }
-            >
-              {item.label || item.value}
-            </Ariakit.SelectItem>
-          ))}
+        <Ariakit.SelectPopover className="mt-2 rounded shadow-lg bg-white dark:bg-surface/90 border border-brand/20 py-2 z-50 animate-fade-in max-h-60 overflow-y-auto">
+          <Ariakit.SelectList>
+            {items.map((item) => (
+              <Ariakit.SelectItem
+                key={item.value}
+                value={item.value}
+                disabled={item.disabled}
+                className={
+                  item.className ||
+                  `block w-full px-4 py-2 text-left rounded transition-colors duration-100 ${item.disabled ? "text-text/40 cursor-not-allowed opacity-60" : "text-text hover:bg-brand/10 focus:bg-brand/10 focus:outline-none capitalize"}`
+                }
+              >
+                {item.label || item.value}
+              </Ariakit.SelectItem>
+            ))}
+          </Ariakit.SelectList>
         </Ariakit.SelectPopover>
       </Ariakit.SelectProvider>
     </div>
